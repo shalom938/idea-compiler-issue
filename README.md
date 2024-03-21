a055c22bb31b22275f373ed6554b8bb109ebea1d
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-03-21 15:01:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# idea-compiler-issue
+
+
+idea complains about junit @MethodSource.
+
+"Cannot resolve target method source: 'allJdks'"
+
+see org.test.idea.issue.MyTests
---
+++ src/test/java/org/test/idea/issue/MyTests.java
@@ -0,0 +1,33 @@
+package org.test.idea.issue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MyTests {
+
+    protected static Stream<Arguments> allJdks() {
+        return IntStream.of(8, 11,17).mapToObj(Arguments::of);
+//        return IntStream.of(8, 11, 17,21).mapToObj(Arguments::of);
+    }
+
+    @ParameterizedTest(name = "jdk {0}")
+    @MethodSource("allJdks")
+    @Retention(RetentionPolicy.RUNTIME)
+    protected @interface TestAllJdks {
+    }
+
+
+    @TestAllJdks
+    public void test(int jdk){
+        System.out.println("testing with "+jdk);
+    }
+
+
+}
